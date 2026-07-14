81a344a300f7edc2e89b5e89392ed4a8d6d2eed6
karentpinzonunir <karent.pinzon126@comunidadunir.net>
2026-07-13 23:03:17 -0500

    permite solo el front hacer post y get

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "https://redraiz.vercel.app" },
+          { key: "Access-Control-Allow-Methods", value: "GET, POST, OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type" },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
